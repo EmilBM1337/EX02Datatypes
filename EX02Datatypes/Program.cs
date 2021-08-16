4e4bab1b9711d2e5bae1150177b7f847dc2e53eb
emil41m5 <emil41m5@CV-BB-5340>
2021-08-16 10:31:14 +0200

    Add project files.

---
+++ EX02Datatypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EX02Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Emil";
+            string lastName = "Mathiesen";
+            int age = 22;
+
+            Console.WriteLine($"Fornavn: {firstName}. Efternavn: {lastName}. Alder: {age}.");
+
+            string title = "Lord of The Rings - The Two Towers";
+            long isbn = 9780007117116;
+            int pages = 450;
+            double price = 249.95;
+
+            Console.WriteLine(title);
+            Console.WriteLine(isbn);
+            Console.WriteLine(pages);
+            Console.WriteLine(price);
+
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+
+            Console.WriteLine(maxInt);
+            Console.WriteLine(minInt);
+        }
+    }
+}
